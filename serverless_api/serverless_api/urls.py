d237dc1982e4422a09deae7858a1b8d43b2446d7
Rakesh <rakeshyadav9698@gmail.com>
2024-11-11 22:24:43 +0530

    missing changes

--- serverless_api/serverless_api/urls.py
+++ serverless_api/serverless_api/urls.py
@@ -16,15 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from api.views import UserViewSet, ProductViewSet
-
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'products', ProductViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('', include('api.urls', namespace='devopsapp')),
 ]
 
