9ed2da62909a1d29e0ae999fbd12f7d24513cbbe
Rakesh <rakeshyadav9698@gmail.com>
2024-11-12 21:45:05 +0530

    cahgnes added

--- serverless_api/serverless_api/settings.py
+++ serverless_api/serverless_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yt1+2!%gicjua$an(y+yn9)mmoduc@t7sh9s#3kg^p=1jj21yw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
